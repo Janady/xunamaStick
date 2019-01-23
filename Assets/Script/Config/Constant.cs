72c316e6c03b5f718d948d15665456bdabfdd5c0
janady <janady@163.com>
2019-01-23 17:07:04 +0800

    Fixed path for file manager

--- Assets/Script/Config/Constant.cs
+++ Assets/Script/Config/Constant.cs
@@ -6,7 +6,11 @@
         public const string VedioPath = "/sdcard/vedioXuanma";
         public const string ImagePath = "/sdcard/Image";
         public const string ExcelPath = "/sdcard/excel";
+#if UNITY_EDITOR
+        public const string UsbPath = "/";
+#else
         public const string UsbPath = "/storage/usbhost1";
+#endif
         public const string Version = "1.0.0";
     }
 }
